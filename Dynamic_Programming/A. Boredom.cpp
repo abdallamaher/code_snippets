0819454f1f4df228fd9e7de9386b3d49dd99a0e5
Abdalla <maher@contactless.io>
2020-05-22 04:09:53 +0200

    A. Boredom

---
+++ Dynamic_Programming/A. Boredom.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WvNINGS
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N = 1e5 + 5;
+int n;
+vector<int> v;
+int cnt[N];
+ll dp[N];
+ll solve(int i=0) {
+    if (i >= n)
+        return 0;
+    if (~dp[i])
+        return dp[i];
+    ll& a = dp[i];
+    a = solve(i + 1);
+    if (v[i]==v[i+1]-1)
+        a = max(a, solve(i + 2) + 1ll * v[i] * cnt[v[i]]);
+    else
+        a = max(a, solve(i + 1) + 1ll * v[i] * cnt[v[i]]);
+
+    return a;
+}
+int main() {
+    cin.sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        if (!cnt[x])
+            v.push_back(x);
+        cnt[x]++;
+    }
+    v.push_back(1e9);
+    n = v.size() - 1;
+    sort(v.begin(), v.end());
+    memset(dp, -1, sizeof(dp));
+    cout << solve();
+    return 0;
+}
