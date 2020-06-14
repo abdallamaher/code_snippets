b5741742e672b4cdbea95104447a3057b3e68a22
Abdalla <maher@contactless.io>
2020-06-14 15:36:58 +0200

    B. Balanced Substring

--- Dynamic_Programming/B. Balanced Substring.cpp
+++ Dynamic_Programming/B. Balanced Substring.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define ll long long
 int n;
 string s;
-map<int, int> dp;
+map<int, int> mp;
 
 int main() {
     cin.sync_with_stdio(false);
@@ -12,17 +12,17 @@ int main() {
     cin >> n >> s;
 
     int cnt = 0, ans = 0;
-    // dp[balace] = index
-    dp[0] = -1;
+    // mp[balace] = index
+    mp[0] = -1;
     for (int i = 0; s[i]; i++) {
         if (s[i] == '1') cnt++;
         else cnt--;
 
         // loop at prefixes in o(n) get timeLimit, and as we need first apperance use map
-        if (dp.find(cnt) == dp.end())
-            dp[cnt] = i;
+        if (mp.find(cnt) == mp.end())
+            mp[cnt] = i;
         else
-            ans = max(ans, i - dp[cnt]);
+            ans = max(ans, i - mp[cnt]);
     }
     cout << ans;
     return 0;
