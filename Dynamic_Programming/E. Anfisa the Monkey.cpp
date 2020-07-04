dd48fce0d01af1bde309462489768139222dc31d
Abdalla <maher@contactless.io>
2020-07-04 08:58:29 +0200

    E. Anfisa the Monkey 1.1

--- Dynamic_Programming/E. Anfisa the Monkey.cpp
+++ Dynamic_Programming/E. Anfisa the Monkey.cpp
@@ -5,18 +5,15 @@ using namespace std;
 int k, a, b, n;
 string s;
 int dp[210][210];
-int path[210][210];
-
 void print(int len, int k) {
     if (len<1)return;
-    int cut = path[len][k];
+    int cut = dp[len][k];
     print(len - cut, k - 1);
     for (int i = len - cut; i < len; i++) {
         cout << s[i];
     }
     cout << '\n';
 }
-
 int main() {
     cin.sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
@@ -28,12 +25,11 @@ int main() {
         for (int j = 0; j <= k; j++) {
             if (dp[i][j] == 0)continue;
             for (int cut = a; cut <= b && i + cut <= n; cut++) {
-                dp[i + cut][j + 1] = 1;
-                path[i + cut][j + 1] = cut;
+                dp[i + cut][j + 1] = cut;
             }
         }
     }
-	
+
     if (dp[n][k])print(n, k);
     else cout << "No solution\n";
     return 0;
