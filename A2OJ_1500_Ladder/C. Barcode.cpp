d8ae31badfb5b4426bb0800951731a54b50d019b
Abdalla <maher@contactless.io>
2020-07-06 01:22:48 +0200

    C. Barcode 1.1

--- A2OJ_1500_Ladder/C. Barcode.cpp
+++ A2OJ_1500_Ladder/C. Barcode.cpp
@@ -1,23 +1,56 @@
-#define _CRT_SECURE_NO_WvNINGS
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-const int N = 1e3 + 5;
 int n, m, x, y;
-char g[N][N];
+const int N = 1e3 + 5;
 int ar[3][N];
-int dp[N][N][2];
-int solve(int i = 0, int len = 0, int col=0) {
+int dp[3][N];
+int mem[N][N][2];
+
+int table() {
+    scanf("%d %d %d %d", &n, &m, &x, &y);
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j <= m; j++) {
+            char c; cin >> c;
+            if (c == '#')ar[0][j]++;
+            else ar[1][j]++;
+        }
+    }
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 1; j <= m; j++) {
+            dp[i][j] = 1e9;
+        }
+    }
+
+    for (int i = m; i > 0; i--) {
+        int sum0 = 0, sum1 = 0;
+        for (int j = i; j > 0; j--) {
+            sum0 += ar[0][j], sum1 += ar[1][j];
+            if (i - j + 1 >= x && i - j + 1 <= y) {
+                dp[0][j] = min(dp[0][j], sum0 + dp[1][i + 1]);
+                dp[1][j] = min(dp[1][j], sum1 + dp[0][i + 1]);
+            }
+        }
+    }
+
+    cout << min(dp[0][1], dp[1][1]);
+    return 0;
+}
+
+
+
+int solve(int i = 0, int len = 0, int col = 0) {
     if (i == m) {
         if (len >= x && len <= y)return 0;
         return 1e9;
     }
-    int& ret = dp[i][len][col];
+    int& ret = mem[i][len][col];
     if (~ret)return ret;
     ret = 1e9;
-    if(!len || len < y)
+    if (!len || len < y)
         ret = min(ret, solve(i + 1, len + 1, col) + n - ar[col ^ 1][i]);
-    if(!len || len >= x)
+    if (!len || len >= x)
         ret = min(ret, solve(i + 1, 1, col ^ 1) + n - ar[col][i]);
     return ret;
 }
@@ -27,19 +60,14 @@ int main() {
     cin >> n >> m >> x >> y;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cin >> g[i][j];
-        }
-    }
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (g[j][i] == '#')ar[0][i]++;
-            else ar[1][i]++;
+            char c; cin >> c;
+            if (c == '#')ar[0][j]++;
+            else ar[1][j]++;
         }
     }
 
-    memset(dp, -1, sizeof(dp));
+    memset(mem, -1, sizeof(mem));
     cout << solve();
     return 0;
 }
-
 
