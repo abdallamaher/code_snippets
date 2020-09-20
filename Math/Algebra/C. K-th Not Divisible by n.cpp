e2c6e6e4f98e88dcc652ae1b4e4c0ef599b947d1
abdallamaher <abdallamaher89@gmail.com>
2020-09-20 19:25:58 +0200

    C. K-th Not Divisible by n

---
+++ Math/Algebra/C. K-th Not Divisible by n.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+
+int main() {
+#ifdef LOCAL   
+	freopen("input.txt", "r", stdin);
+#endif
+	int t, n, d; scanf("%d", &t);
+	while (t--) {
+		scanf("%d%d", &d, &n);
+		ll ans = 1ll * n * d / (d - 1);
+		if (ans % d == 0)ans--;
+		printf("%lld\n", ans);
+	}
+	return 0;
+}
