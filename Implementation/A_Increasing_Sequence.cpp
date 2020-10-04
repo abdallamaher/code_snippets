6aa990dcbd3673c5edac176281f46b3bea5f6e89
abdallamaher <abdallamaher89@gmail.com>
2020-10-04 21:11:07 +0200

    A_Increasing_Sequence.cpp

--- Implementation/A_Increasing_Sequence.cpp
+++ Implementation/A_Increasing_Sequence.cpp
@@ -8,17 +8,11 @@ int main() {
     for (auto& it : v)scanf("%d", &it);
     ll ans = 0;
     for (int i = 1; i < n; i++) {
-        //printf("\n%d %d\n", v[i - 1], v[i]);
         if (v[i] <= v[i - 1]) {
             int t = (v[i - 1] - v[i] + k) / k;
             v[i] += t * k;
             ans += t;
         }
-        // if (v[i] == v[i - 1]) {
-        //     v[i] += k;
-        //     ++ans;
-        // }
-        //printf("%d %d\n", v[i - 1], v[i]);
     }
     printf("%lld", ans);
     return 0;
