378c1b1dd1e0e2b4e8da83c71b38920723d3367c
abdallamaher <abdallamaher89@gmail.com>
2020-10-06 17:23:09 +0200

    A_Exams.cpp

---
+++ Implementation/A_Exams.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+
+int main() {
+    int n, m; scanf("%d%d", &n, &m);
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            for (int a = 0; a <= n; a++) {
+                for (int b = 0; b <= n; b++) {
+                    if (i + j + a + b == n && i * 2 + j * 3 + a * 4 + b * 5 == m) {
+                        return printf("%d\n", i) * 0;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
