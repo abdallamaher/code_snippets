e7a59fa7a8c011bea6107ce1558e18dc3e5852d5
abdallamaher <abdallamaher89@gmail.com>
2020-10-12 12:43:23 +0200

    A_Polo_the_Penguin_and_Segments.cpp

---
+++ Implementation/A_Polo_the_Penguin_and_Segments.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    int n, k; scanf("%d%d", &n, &k);
+    ll sum = 0;
+    while (n--) {
+        int l, r; scanf("%d%d", &l, &r);
+        sum += (r - l + 1);
+    }
+    printf("%lld\n", sum % k == 0 ? 0 : k - (sum + k) % k);
+    return 0;
+}
