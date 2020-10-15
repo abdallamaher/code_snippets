e76bdc8ba09d91d2b557cfe8edb05ea748e4c043
abdallamaher <abdallamaher89@gmail.com>
2020-10-15 15:35:48 +0200

    B_Vanya_and_Lanterns.cpp

---
+++ Implementation/B_Vanya_and_Lanterns.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ld long double
+
+int main() {
+    int n, l; cin >> n >> l;
+    vector<int> v(n);
+    for (auto& it : v)scanf("%d", &it);
+    sort(v.begin(), v.end());
+    int a = 0, b = max(v[0], l - v[n - 1]);
+    for (int i = 1; i < n; i++) {
+        a = max(a, v[i] - v[i - 1]);
+    }
+    long double ans = max(a / 2.0L, 1.0L * b);
+    cout << fixed;
+    cout << setprecision(10) << ans << endl;
+    return 0;
+}
