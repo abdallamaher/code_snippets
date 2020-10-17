94c1025dcfdc15c97847f26a0111c0703158acce
abdallamaher <abdallamaher89@gmail.com>
2020-10-18 00:21:18 +0200

    C_ABBB.cpp

--- Implementation/C_ABBB.cpp
+++ Implementation/C_ABBB.cpp
@@ -7,15 +7,14 @@ int main() {
 	int t; cin >> t;
 	while (t--) {
 		string s; cin >> s;
-		for (int i = 0; i < s.size(); i++) {
-			if (i < s.size() - 1 && (s[i] == 'A' || s[i] == 'B') && s[i + 1] == 'B') {
-				s.erase(i, 2);
-				if(i)i-=2;
-				else i--;
+		string ans;
+		for (auto c : s) {
+			if (c == 'B' && ans.size() && (ans.back() == 'A' || ans.back() == 'B')) {
+				ans.pop_back();
 			}
+			else ans.push_back(c);
 		}
-		cout << s.size() << '\n';
+		cout << ans.size() << '\n';
 	}
-
 	return 0;
 }
