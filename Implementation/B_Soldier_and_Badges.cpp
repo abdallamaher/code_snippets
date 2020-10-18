6cf2858b5ba2d8fc2254a43432428c83a44f59d2
abdallamaher <abdallamaher89@gmail.com>
2020-10-18 10:56:05 +0200

    B_Soldier_and_Badges.cpp

---
+++ Implementation/B_Soldier_and_Badges.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    int n; scanf("%d", &n);
+    vector<int> v(n);
+    for (auto& it : v)scanf("%d", &it);
+    sort(v.begin(), v.end());
+    ll sum = 0;
+    for (int i = 1; i < n; i++) {
+        if (v[i] <= v[i - 1]) {
+            sum += v[i - 1] + 1 - v[i];
+            v[i] = v[i - 1] + 1;
+        }
+    }
+    cout << sum << '\n';
+    return 0;
+}
