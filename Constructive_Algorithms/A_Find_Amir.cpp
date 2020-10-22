21cd04606c2d5569638b4f76dac0cd27b5de5c3c
abdallamaher <abdallamaher89@gmail.com>
2020-10-22 21:51:31 +0200

    A_Find_Amir.cpp

---
+++ Constructive_Algorithms/A_Find_Amir.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+
+int main() {
+    int n; 
+    cin >> n;
+    cout << n - ((n + 2) / 2);
+	return 0;
+}
