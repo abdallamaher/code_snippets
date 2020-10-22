1fbf1163289b4a406c6d9715d1a4573903f3cb09
abdallamaher <abdallamaher89@gmail.com>
2020-10-22 11:59:40 +0200

    B_Lovely_Palindromes.cpp

---
+++ Constructive_Algorithms/B_Lovely_Palindromes.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+int main() {
+    string s; cin >> s;
+    cout << s;
+    reverse(s.begin(), s.end());
+    cout << s;
+    return 0;
+}
