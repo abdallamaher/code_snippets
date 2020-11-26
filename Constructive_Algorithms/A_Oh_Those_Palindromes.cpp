84428a52aef182674ad788b93f83c65fd73814ec
abdallamaher <abdallamaher89@gmail.com>
2020-11-26 02:34:48 +0200

    A_Oh_Those_Palindromes.cpp

---
+++ Constructive_Algorithms/A_Oh_Those_Palindromes.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+int main() {
+    int n; string s; 
+    cin >> n >> s;
+    sort(s.begin(), s.end());
+    cout << s;
+    return 0;
+}
