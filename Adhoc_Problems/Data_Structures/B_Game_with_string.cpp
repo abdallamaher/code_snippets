32f8ef9bb6bec6f39d5c22de99020f97efe43a4e
abdallamaher <abdallamaher89@gmail.com>
2020-12-01 03:34:42 +0200

    B_Game_with_string.cpp

--- Adhoc_Problems/Data_Structures/B_Game_with_string.cpp
+++ Adhoc_Problems/Data_Structures/B_Game_with_string.cpp
@@ -3,7 +3,25 @@
 using namespace std;
 #define ll long long 
 
+inline void solve() {
+    string s; cin >> s;
+    int lose = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == s[i + 1]) {
+            s.erase(i, 2);
+            if (!i)i++;
+            i -= 2;
+            lose = 1 - lose;
+        }
+    }
+    puts(lose ? "Yes" : "No");
+}
+
 int main() {
+
+    solve();
+    return 0;
+
     string s; cin >> s;
     vector<char> v;
     int lose = 0;
