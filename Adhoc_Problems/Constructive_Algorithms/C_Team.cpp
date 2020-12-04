7b39fa00f3592cdf0a9678736f889cfe700eab50
abdallamaher <abdallamaher89@gmail.com>
2020-12-04 07:21:39 +0200

    C_Team.cpp

---
+++ Adhoc_Problems/Constructive_Algorithms/C_Team.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    int z, o; cin >> z >> o;
+    int ok = 0;
+    if (z >= o) ok = bool(z - o > 1);   // 010101
+    else ok = bool(o > z * 2 + 2);  // 110110 11
+    if (ok) {
+        puts("-1");
+        return 0;
+    }
+
+    string s;
+    while (o || z) {
+        if (o && o >= z) {
+            s += '1';
+            o--;
+        }
+        if (o && o >= z) {
+            s += '1';
+            o--;
+        }
+        if (z) {
+            s += '0';
+            z--;
+        }
+    }
+    if (s.find("111") != -1 || s.find("00") != -1)
+        puts("-1");
+    else
+        cout << s << endl;
+}
