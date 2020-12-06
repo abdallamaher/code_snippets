f3bb78c13920b5a4fa606f2714bc0e982bf5e0ae
abdallamaher <abdallamaher89@gmail.com>
2020-12-06 20:38:56 +0200

    A_Grid_game.cpp

--- Adhoc_Problems/Constructive_Algorithms/A_Grid_game.cpp
+++ Adhoc_Problems/Constructive_Algorithms/A_Grid_game.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 
 int main() {
-    vector<pair<int, int>> v = { {1,1}, {3, 1} };
-    vector<pair<int, int>> h = { {1, 3}, {2, 3},{3, 3},{4, 3} };
+    vector<int> v = { 1, 3 };
+    vector<int> h = { 1, 2, 3, 4 };
     string s; cin >> s;
     int a = 0, b = 0;
     for (auto it : s) {
         if (it == '0') {
-            printf("%d %d\n", v[a % 2].first, v[a % 2].second);
+            printf("%d %d\n", v[a % 2], 1);
             a++;
         }
         else {
-            printf("%d %d\n", h[b % 4].first, h[b % 2].second);
+            printf("%d %d\n", h[b % 4], 3);
             b++;
         }
     }
