2d53d7b0d27132cb7872ea94a5a4c05b064fd9d1
abdallamaher <abdallamaher89@gmail.com>
2020-12-06 17:44:06 +0200

    C_Palindromifier.cpp

---
+++ Adhoc_Problems/Constructive_Algorithms/C_Palindromifier.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    string s; cin >> s;
+    cout << 3 << endl;
+    printf("L 2\nR 2\nR %d\n", 2 * s.length() - 1);
+}
