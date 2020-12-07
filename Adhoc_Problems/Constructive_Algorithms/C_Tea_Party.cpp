70196a9ff88b492d7ae7b4456988c9279c732895
abdallamaher <abdallamaher89@gmail.com>
2020-12-08 00:33:48 +0200

    C_Tea_Party.cpp

--- Adhoc_Problems/Constructive_Algorithms/C_Tea_Party.cpp
+++ Adhoc_Problems/Constructive_Algorithms/C_Tea_Party.cpp
@@ -7,37 +7,29 @@ using namespace std;
 int main() {
     int n, rem; cin >> n >> rem;
     int sum = 0;
-    int tmp = rem;
-    vector<pair<int, int>> v(n), ans(n);
+    vector<pair<int, int>> v(n);
     for (int i = 0; i < n; i++) {
         cin >> v[i].first;
         v[i].second = i;
-        int x = (v[i].first + 1) / 2;
-        rem -= x;
-        sum += v[i].first - x;
+        sum += (v[i].first + 1) / 2;
     }
-    if (rem < 0)cout << -1;
-    else if (rem > sum)cout << -1;
+    if (rem - sum < 0)cout << -1;
     else {
+        vector<int> ans(n + n);
         sort(v.rbegin(), v.rend());
         for (int i = 0; i < n;i++) {
             int x = (v[i].first + 1) / 2;
-            ans[i].first += x;
-            ans[i].second = v[i].second;
+            ans[v[i].second] += x;
             v[i].first -= x;
-            tmp -= x;
+            rem -= x;
         }
-        for (int i = 0; tmp > 0 && i < n;i++) {
-            int x = min(v[i].first, tmp);
-            ans[i].first += x;
-            tmp -= x;
+        for (int i = 0; rem > 0 && i < n;i++) {
+            int x = min(v[i].first, rem);
+            ans[v[i].second] += x;
+            rem -= x;
         }
-        sort(ans.begin(), ans.end(), [](pair<int, int> f, pair<int, int> s) {
-            return f.second < s.second;
-            });
         for (int i = 0;i < n;i++) {
-            cout << ans[i].first << ' ';
+            cout << ans[i] << ' ';
         }
-
     }
 }
