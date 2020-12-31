ee18045c26fd201d4924cc318c067580044dcc39
abdallamaher <abdallamaher89@gmail.com>
2020-12-31 11:08:37 +0200

    B. DZY Loves Chemistry.cpp

--- Graph/B. DZY Loves Chemistry.cpp
+++ Graph/B. DZY Loves Chemistry.cpp
@@ -1,36 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-int n, m, x, y;
-vector<int> adj[55];
-int vis[55];
+const int N = 1e4;
+vector<int> adj[N];
+int vis[N];
 
 int dfs(int i) {
-	vis[i] = 1;
-	int ans = 0;
-	for (auto it : adj[i])if (!vis[it])
-		ans += dfs(it) + 1;
-	return ans;
+    vis[i] = 1;
+    int ret = 0;
+    for (auto e : adj[i]) if (!vis[e]) {
+        ret += 1 + dfs(e);
+    }
+    return ret;
 }
 
 int main() {
-	cin.sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-	cin >> n >> m;
-	for (int i = 0; i < m; i++) {
-		cin >> x >> y;
-		adj[x].push_back(y);
-		adj[y].push_back(x);
-	}
-	int ans = 0;
-	for (int i = 1; i <= n; i++) {
-		if (!vis[i]) {
-			ans += dfs(i);
-			//cerr << i << "*********" << x << endl;
-		}
-	}
-	cout << (1LL << ans) << '\n';
-	return 0;
+    int n, m; scanf("%d%d", &n, &m);
+    for (int i = 0; i < m; i++) {
+        int u, v; scanf("%d %d", &u, &v);
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i]) {
+            cnt += dfs(i);
+        }
+    }
+    cout << (1LL << cnt) << endl;
 }
-
-
