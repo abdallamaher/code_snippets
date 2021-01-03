17f543de78afe4d103409b464e9f563f0e3bd156
abdallamaher <abdallamaher89@gmail.com>
2021-01-03 14:28:43 +0200

    B_Minimum_number_of_steps.cpp

---
+++ Math/Combinatorics_Permutations/B_Minimum_number_of_steps.cpp
@@ -0,0 +1,19 @@
+git add#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    string s; cin >> s;
+    const int mod = 1e9 + 7;
+    ll b = 0, ans = 0;
+    for (int i = s.size() - 1; i >= 0;i--) {
+        if (s[i] == 'b')b++;
+        else {
+            ans += b;
+            if (ans > mod)ans %= mod;
+            b *= 2;
+            if (b > mod)b %= mod;
+        }
+    }
+    cout << ans << endl;
+}
