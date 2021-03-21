5e8e948fd912c9f3cf2487c8fdc9dfc7b8398c7f
abdalla maher <abdallamaher89@gmail.com>
2021-03-21 18:17:54 +0200

    AGGRCOW - Aggressive cows.cpp

--- Binary_Search/AGGRCOW - Aggressive cows.cpp
+++ Binary_Search/AGGRCOW - Aggressive cows.cpp
@@ -1,38 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-int t, n, c;
-const int N = 1e5 + 5;
+int n, c;
+const int N = 2e5 + 5;
 int ar[N];
 
-bool can(int x) {
-	int cnt = 0, p = 0;
-	for (int i = 1; i < n; i++) {
-		if (ar[i] - ar[p] >= x)p = i, cnt++;
-	}
-	return cnt >= c-1;
+inline bool can(int dis) {
+    int pre = 0, cnt = 1;
+    for (int i = 1; i < n;i++) {
+        if (ar[i] - ar[pre] >= dis)pre = i, cnt++;
+    }
+    return cnt >= c;
+}
+
+inline void solve() {
+    scanf("%d %d", &n, &c);
+    for (int i = 0;i < n;i++)scanf("%d", ar + i);
+    sort(ar, ar + n);
+    int st = 1, en = 1e9 + 5, mid;
+    while (st < en) {
+        mid = (st + en + 1) / 2;
+        if (can(mid)) st = mid;
+        else en = mid - 1;
+    }
+    printf("%d\n", en);
 }
 
 int main() {
-#ifdef LOCAL   
-	freopen("input.txt", "r", stdin);
-#endif
-	scanf("%d", &t);
-	while (t--) {
-		scanf("%d %d", &n, &c);
-		for (int i = 0; i < n; i++) {
-			scanf("%d", ar + i);
-		}
-		sort(ar, ar + n);
-		int st = 0, en = 1e9, mid;
-		while (st < en) {
-			mid = (st + en + 1) >> 1;
-			if (can(mid))
-				st = mid;
-			else
-				en = mid - 1;
-		}
-		printf("%d\n", en);
-	}
-	return 0;
+    int t; cin >> t;
+    while (t--)solve();
 }
