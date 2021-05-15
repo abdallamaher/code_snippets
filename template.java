d663fc6342b2cfae9845f08de57e6b62e514c2b2
abdalla maher <abdallamaher89@gmail.com>
2021-05-15 15:24:53 +0200

    template.java

---
+++ template.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+ 
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		Scanner sc = new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		
+		out.close();
+	}
+ 
+	static class Scanner
+	{
+		StringTokenizer st;
+		BufferedReader br;
+ 
+		public Scanner(InputStream s){	br = new BufferedReader(new InputStreamReader(s));}
+ 
+		public Scanner(String s) throws FileNotFoundException {	br = new BufferedReader(new FileReader(s));}
+ 
+		public String next() throws IOException
+		{
+			while (st == null || !st.hasMoreTokens())
+				st = new StringTokenizer(br.readLine());
+			return st.nextToken();
+		}
+ 
+		public int nextInt() throws IOException {return Integer.parseInt(next());}
+ 
+		public long nextLong() throws IOException {return Long.parseLong(next());}
+ 
+		public String nextLine() throws IOException {return br.readLine();}
+ 
+		public double nextDouble() throws IOException { return Double.parseDouble(next()); }
+ 
+		public boolean ready() throws IOException {return br.ready();}
+
+	}
+}
