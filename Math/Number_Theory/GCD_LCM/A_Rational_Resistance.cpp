f8df829b324fde6a8ac91b7e30d96933cd55d3c4
abdalla maher <abdallamaher89@gmail.com>
2021-07-28 19:30:37 +0200

    Number_Theory/GCD_LCM/A_Rational_Resistance.cpp

--- Math/Number_Theory/GCD_LCM/A_Rational_Resistance.cpp
+++ Math/Number_Theory/GCD_LCM/A_Rational_Resistance.cpp
@@ -7,11 +7,13 @@ int main() {
     ll ans = 0;
     /*
     8/3 become 2 + 2/3 become 2 + 1/(3/2)
-    
-    make 1/(3/2)
-    1/(1 + (1/2))
+    make (3/2)      become (1 + (1/2))
+    make (1/2)      become 2
 
-    make (1/2)
+
+    199/200 become 1/(200/199)
+    make (200/199)  become 1 + (1/199)
+    make (1/199)    become 199
     */
     while (b) {
         ans += a / b;
