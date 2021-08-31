9b26a566cff51b715d9daa973d2032c3510e7693
abdalla maher <abdallamaher89@gmail.com>
2021-08-31 07:48:53 +0200

    Algebra/Number_System/A_Digits_Sum.cpp

---
+++ Math/Algebra/Number_System/A_Digits_Sum.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+inline void solve() {
+    int n; scanf("%d", &n);
+    cout << (n + 1) / 10 << endl;
+}
+
+int main() {
+    int t; cin >> t;
+    while (t--)solve();
+}
