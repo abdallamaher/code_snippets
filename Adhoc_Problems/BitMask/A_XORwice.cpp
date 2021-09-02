55d03bb82e8fbe163e607fe2c01abd30971ab934
abdalla maher <abdallamaher89@gmail.com>
2021-09-02 21:28:46 +0200

    BitMask/A_XORwice.cpp

--- Adhoc_Problems/BitMask/A_XORwice.cpp
+++ Adhoc_Problems/BitMask/A_XORwice.cpp
@@ -1,23 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define ld long double
 
+inline void solve() {
+    int a, b; cin >> a >> b;
+    cout << (a ^ b) << endl;
+}
+
+inline void run() {
+    int a, b; cin >> a >> b;
+    int ans = 0;
+    for (int i = 0; i < 30;i++) {
+        int bit_a = (a & (1 << i));
+        int bit_b = (b & (1 << i));
+        if (bit_a || bit_b)ans |= (1 << i);
+    }
+    cout << (a ^ ans) + (b ^ ans) << endl;
+}
+
+string to_binary(int n) {
+    string s;
+    while (n > 0) {
+        s += (n & 1) ? '1' : '0';
+        n >>= 1;
+    }
+    while (s.size() < 30)s += '0';
+    // reverse(s.begin(), s.end());
+    return s;
+}
+
+inline void go() {
+    int a, b; cin >> a >> b;
+    string s = to_binary(a);
+    string t = to_binary(b);
+    int ans = 0;
+    for (int i = 0;i < 30;i++) {
+        if (s[i] != t[i]) {
+            ans = (ans | (1 << i));
+        }
+    }
+    cout << ans << endl;
+}
 int main() {
-	int t; cin >> t;
-	while (t--) {
-		int a, b; cin >> a >> b;
-		int ans = 0;
-		for (int i = 0; i <= 30; i++) {
-			if (( a>>i & 1) && ( b>>i & 1))ans |= 1<<i;
-		}
-		cout << (a ^ ans) + (b ^ ans) << '\n';
-	}
-
-	return 0;
-	while (t--) {
-		int a, b; cin >> a >> b;
-		cout << (b ^ a) << '\n';
-	}
-	return 0;
+    int t; cin >> t;
+    while (t--)run();
 }
