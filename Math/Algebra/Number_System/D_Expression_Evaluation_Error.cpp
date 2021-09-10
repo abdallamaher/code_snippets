0af71ab34da10f393f46d4db349a4e191929aff1
abdalla maher <abdallamaher89@gmail.com>
2021-09-10 15:35:17 +0200

    update template.cpp

--- Math/Algebra/Number_System/D_Expression_Evaluation_Error.cpp
+++ Math/Algebra/Number_System/D_Expression_Evaluation_Error.cpp
@@ -7,8 +7,6 @@ inline void solve() {
 }
  
 int main() {
-    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-    int t = 1;
-    cin >> t;
+    int t; cin >> t;
     while (t--)solve();
 }
