b4d0bef920830c106151be0fa497f46ee7d24d4e
abdallamaher <abdallamaher89@gmail.com>
2022-01-01 19:42:32 +0200

    String_Processing/Trie/Phone_List.cpp

--- String_Processing/Trie/Phone_List.cpp
+++ String_Processing/Trie/Phone_List.cpp
@@ -2,53 +2,31 @@
 using namespace std;
 #define ll long long
 
-vector<vector<int>> v;
+vector< vector<int> > v;
 vector<int> e;
 
-
 void insert(string& s) {
-    int cur = 0;
-    for (int i = 0; i < (int)s.size(); ++i) {
-        char c = s[i] - '0';
-        if (v[cur][c] == -1) {
-            v[cur][c] = v.size();
+    int node = 0;
+    for (int i = 0;i < (int)s.size(); i++) {
+        int c = s[i] - '0';
+        if (v[node][c] == -1) { //stand on node and want to c
+            v[node][c] = v.size();
             v.push_back(vector<int>(10, -1));
             e.push_back(false);
         }
-        cur = v[cur][c];
+        node = v[node][c];
     }
-    e[cur] = true;
+    e[node] = true;
 }
 
-bool find(string& s) {
-    int cur = 0;
-    for (int i = 0; i < (int)s.size(); ++i) {
-        char c = s[i] - '0';
-        if (v[cur][c] == -1) return false;
-        cur = v[cur][c];
+bool is_found(string& s) {
+    int node = 0;
+    for (int i = 0; i < (int)s.size(); i++) {
+        int c = s[i] - '0';
+        if (v[node][c] == -1)return false;
+        node = v[node][c];
     }
     return true;
-}
-
-inline void solve() {
-    int n; cin >> n;
-    vector<string> vec;
-    for (int i = 0; i < n; ++i) {
-        string s; cin >> s;
-        vec.push_back(s);
-    }
-    sort(vec.begin(), vec.end(), [](string& a, string& b) {
-        return (b.size() < a.size());
-        });
-
-    for (int i = 0; i < n; ++i) {
-        if (find(vec[i])) {
-            cout << "NO\n";
-            return;
-        }
-        insert(vec[i]);
-    }
-    cout << "YES\n";
 }
 
 void init() {
@@ -60,7 +38,25 @@ void init() {
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-    int t = 1;
-    cin >> t;
-    while (t--)init(), solve();
+    int T; cin >> T;
+    while (T--) {
+        init();
+        int n; cin >> n;
+        vector<string> vec;
+        for(int i=0; i<n;i++){
+            string s; cin >> s;
+            vec.push_back(s);
+        }
+
+        sort(vec.begin(), vec.end(), [](string& f, string &s){
+            return (int)f.size() > (int)s.size();
+        });
+
+        bool ok = false;
+        for (int i = 0; i < n; i++) {
+            ok |= is_found(vec[i]);
+            insert(vec[i]);
+        }
+        cout << (!ok ? "YES" : "NO") << endl;
+    }
 }
