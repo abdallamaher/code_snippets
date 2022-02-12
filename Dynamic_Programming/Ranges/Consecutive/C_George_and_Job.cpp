aaabbb5173842516b2cde2e7f2f00dd33e0af598
abdalla maher <abdallamaher89@gmail.com>
2022-02-13 01:34:59 +0200

    Dynamic_Programming/Ranges/Consecutive/C_George_and_Job.cpp

--- Dynamic_Programming/Ranges/Consecutive/C_George_and_Job.cpp
+++ Dynamic_Programming/Ranges/Consecutive/C_George_and_Job.cpp
@@ -2,34 +2,29 @@
 using namespace std;
 #define ll long long
 
-const int N = 5e3 + 5;
+const int N = 5e3 + 10;
 int n, m, k;
-ll dp[N][N];
 ll ar[N];
+ll dp[N][N];
 
-ll go(int i, int rem) {
-    if (i > n)return -1e15;
-    if (i == n || rem == k)return 0;
+ll solve(int i, int rem) {
+    if (i > n || rem == 0)return 0;
     ll& ret = dp[i][rem];
     if (~ret)return ret;
-    ll leave = go(i + 1, rem);
-    ll take = go(i + m, rem + 1) + ar[i + m - 1] - ar[i - 1];
+    ll leave = solve(i + 1, rem);
+    ll take = -1e14;
+    if (i + m - 1 <= n)
+        take = solve(i + m, rem - 1) + ar[i + m - 1] - ar[i - 1];
     return ret = max(leave, take);
-}
-
-inline void solve() {
-    cin >> n >> m >> k;
-    for (int i = 0; i < n; i++) {
-        cin >> ar[i];
-        if (i)ar[i] += ar[i - 1];
-    }
-    memset(dp, -1, sizeof(dp));
-    cout << go(0, 0);
 }
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-    int t = 1;
-    // cin >> t;
-    while (t--)solve();
+    cin >> n >> m >> k;
+    for (int i = 1; i <= n; i++) {
+        cin >> ar[i];
+        ar[i] += ar[i - 1];
+    }
+    memset(dp, -1, sizeof(dp));
+    cout << solve(1, k) << endl;
 }
